819c184a06eae333481ac30c2ba0fb78301dbf5d
alternatekev <alternatekev@me.com>
2017-03-29 14:05:55 -0600

    working blank page

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _getPrototypeOf = require('next/node_modules/babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require('next/node_modules/babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('next/node_modules/babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require('next/node_modules/babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('next/node_modules/babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/alternatekev/Documents/alternate-react/pages/index.js?entry';
+
+
+var AlternateIndex = function (_React$Component) {
+  (0, _inherits3.default)(AlternateIndex, _React$Component);
+
+  function AlternateIndex() {
+    (0, _classCallCheck3.default)(this, AlternateIndex);
+
+    return (0, _possibleConstructorReturn3.default)(this, (AlternateIndex.__proto__ || (0, _getPrototypeOf2.default)(AlternateIndex)).apply(this, arguments));
+  }
+
+  (0, _createClass3.default)(AlternateIndex, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement('div', {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 7
+        }
+      });
+    }
+  }]);
+
+  return AlternateIndex;
+}(_react2.default.Component);
+
+exports.default = AlternateIndex;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AlternateIndex extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default AlternateIndex;
